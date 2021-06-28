0de67278a97a077400ced7af33f4f5a5f1ab718c
MNI Niro <mniniro@hotmail.com>
2021-06-28 13:05:55 +0600

    Add files via upload

--- Part-53-Class vs function inheritance/class.js
+++ Part-53-Class vs function inheritance/class.js
@@ -4,47 +4,77 @@
 //which is a prototype-based inheritance language.
 
 // Initializing a class definition
-class Hero {
-    constructor(name, level) {
+class Parents {
+    constructor(name, level) { // to initialize an object
         this.name = name;
         this.level = level;
+        this.point = function(){
+            return "Instance Member"
+        }
     }
-
-    // Adding a method to the constructor
-    greet() {
-        return `${this.name} says hello.`;
+    // Adding a property to the constructor
+    info() {
+        return `${this.name} in level ${this.level}`;
     }
 }
 
 // Adding a method to the constructor
-Hero.prototype.greet = function() {
-    return `${this.name} says hello.`;
+Parents.prototype.greet = function() {
+    return `${this.name} Prototype Member`;
 }
 
-const hero1 = new Hero('Varg', 1);
+//Object creation
+const hero1 = new Parents('Varg', 1);
 
+document.write('From class.js','<br>')
+document.write('==== Parent Class =====','<br>')
 console.log(hero1)
-document.write(hero1.name,'<br>')
-document.write(hero1.level,'<br>')
-document.write(hero1.greet())
+document.write(hero1.info(),'<br>') //calling property of the parent class
+document.write('Name: ',hero1.name,'<br>')
+document.write('Level: ',hero1.level,'<br>')
+document.write('Points: ',hero1.point(),'<br>')
+document.write(hero1.greet(),'<br>','<br>') //calling prototype
 
 
-// Creating a new class from the parent
-class Mage extends Hero {
+//============ Creating a new Child class from the parent class ===========
+class Child extends Parents {
     constructor(name, level, spell) {
         // Chain constructor with super
-        super(name, level);
-
-        // Add a new property
+        super(name, level); //to call & initialize  Parents class. Must use before 'this'
+        // Adding a new property in the Child class
         this.spell = spell;
     }
+    // Adding a property to the constructor
+    Chinfo() {
+        return `${this.spell}`;
+    }
 }
-const hero2 = new Mage('Lejon', 2, 'Magic Missile');
 
+// Adding a method to the Child constructor
+Child.prototype.Chgreet = function() {
+    return `${this.spell} has a Prototype Member`;
+}
+
+const hero2 = new Child('Lejon', 2, 'Magic Missile');
+
+document.write('==== Child Class =====')
 console.log(hero1)
 document.write('<br>')
-document.write(hero2.level,'<br>')
-document.write(hero2.spell,'<br>')
-document.write(hero1.name,'<br>') //from super class
-document.write(hero2.name)
+document.write('Level: ',hero2.level,'<br>')
+document.write('Spell: ',hero2.spell,'<br>')
+document.write('Parent class name: ',hero1.name,'<br>') //from super class
+document.write('Child class name: ',hero2.name,'<br>')
+document.write('Child class method: ',hero2.Chinfo(),'<br>')
+document.write('Child class Prototype: ',hero2.Chgreet(),'<br>')
+document.write('Parents class Prototype: ',hero2.greet(),'<br>')//from Super class
 
+//Method Overriding in JS
+class GrandChild extends Child {
+    //this GrandChild's Chinfo method will override Child Class's Chinfo method
+    Chinfo(){
+        return "Grand Child class method"
+    }
+}
+
+var gc = new GrandChild();
+document.write(gc.Chinfo());
--- Part-53-Class vs function inheritance/function.js
+++ Part-53-Class vs function inheritance/function.js
@@ -9,24 +9,27 @@ var Zero = function(name,level){
 Zero.prototype.greet = function() {
     return `${this.name} says hello.`;
 }
-const zero1 = new Zero('MNI', 10);
 
+//Object creation
+const zero1 = new Zero('MNI', 10);
+document.write('From function.js','<br>')
+document.write('==== Parent Class =====','<br>')
 console.log(zero1)
 document.write(zero1.name,'<br>')
 document.write(zero1.level,'<br>')
-document.write(zero1.greet())
+document.write(zero1.greet(),'<br>','<br>')
 
 // Creating a new constructor from the parent
 //function Gage(name, level, spell) { //following is the alternet way
 var Gage = function(name, level, spell) {
     // Chain constructor with call
     Zero.call(this, name, level);
-
     this.spell = spell;
 }
 
 const zero2 = new Gage('Niro', 20, 'Black Flag');
 
+document.write('==== Child Class =====')
 console.log(zero2)
 document.write('<br>')
 document.write(zero2.level,'<br>')
